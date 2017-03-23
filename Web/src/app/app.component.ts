f07b46a8437799a96bfd1df70cacce6c07a50e2e
David Hardy <david.hardy@codecentric.nl>
2017-03-23 13:14:59 +0100

    Added view to frontend

--- Web/src/app/app.component.ts
+++ Web/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 import {FireEvent} from "./model/fire-event";
+import {FireView} from "./model/fire-view";
 
 @Component({
   selector: 'app-root',
@@ -8,13 +9,18 @@ import {FireEvent} from "./model/fire-event";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello app works!';
+  title = 'Hollywood';
 
   events: FireEvent[];
+  view: FireView;
 
   constructor(af: AngularFire) {
     af.database.list('/events').subscribe(events => {
       this.events = events
+    });
+
+    af.database.object('/views').subscribe(view => {
+      this.view = view
     });
   }
 }
---
+++ Web/src/app/model/fire-view.ts
@@ -0,0 +1,9 @@
+export class FireView {
+  appId: string;
+  checked: Map<string, number>;
+  clientType: Map<string, number>;
+  eventCount: number;
+  radio: Map<string, number>;
+  sliderAverage: number;
+  users: number;
+}
